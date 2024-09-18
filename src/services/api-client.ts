b3da41743c69c7261b908270a8a2548a868b703e
kyawThetHtun <kyawthe235@gmail.com>
2024-09-18 22:51:04 +0630

    fetching genres

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
-  },
-});
+  baseURL: 'https://api.rawg.io/api',
+  params:{
+    key: '99bb46cf2ebc45d49f0560519e1b7669'
+  }
+})
