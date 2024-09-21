831c3ec5eeecaa5167d6446abffa20173bf11e9a
kyawThetHtun <kyawthe235@gmail.com>
2024-09-22 00:10:30 +0630

    ms add

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import APICLENT from "../services/api-client";
 import platforms from "../data/platforms";
 import { Platform } from "./useGames";
+import ms from "ms";
 
 const apiClient = new APICLENT<Platform>('/platforms/lists/parents')
 
@@ -9,7 +10,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms('24h'),
     initialData: { count: platforms.length, results: platforms }
   });
 
